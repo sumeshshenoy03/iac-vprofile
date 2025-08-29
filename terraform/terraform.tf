b2216b3641d0975a56c47e45623c420316f94d76
devops4sure <devops4sure@gmail.com>
2025-08-29 13:33:51 +0400

    workflow rev-2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofileactions2485"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
